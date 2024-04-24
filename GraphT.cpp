e02b0bbcf650606358a27bcaf34aea32295d0520
DrErickson <77122859+DrErickson@users.noreply.github.com>
2024-04-24 09:39:07 -0700

    Update GraphT.cpp

--- GraphT.cpp
+++ GraphT.cpp
@@ -8,41 +8,23 @@ GraphT<T>::GraphT() {
 
 template<class T>
 void GraphT<T>::AddVertex(string vertex) {
-    vertices.insert(vertex);
+   
 }
 
 template<class T>
 void GraphT<T>::AddEdge(string vertexA, string vertexB, T weight) {
-    if (vertices.count(vertexA) == 0 || vertices.count(vertexB) == 0) {
-        throw VertexNotFound();
-    }
-//    
-    pair<string, T> edgeWeightPair;
-    edgeWeightPair.first = vertexB;
-    edgeWeightPair.second = weight;
-    graph[vertexA].insert(edgeWeightPair);
+
 }
 
 template<class T>
-T* GraphT<T>::GetWeight(string vertexA, string vertexB) {
-    if (vertices.count(vertexA) == 0 || vertices.count(vertexB) == 0) {
-        throw VertexNotFound();
-    }
+T GraphT<T>::GetWeight(string vertexA, string vertexB) {
     
-    if (graph[vertexA].count(vertexB) == 0) {
-        return nullptr;
-    }
-    
-    return new T(graph[vertexA][vertexB]);
+    return T();
 }
 
 template<class T>
 queue<string> GraphT<T>::GetToVertices(string vertex) {
     queue<string> adjacentVerticies;
-
-    for (auto it = graph[vertex].begin(); it != graph[vertex].end(); ++it) {
-        adjacentVerticies.push(it->first);
-    }
     
     return adjacentVerticies;
 }
